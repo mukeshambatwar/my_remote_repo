242611eb5a6a7d7db444a8cbda09bef01dab6fe6
unknown <mukesh.ambatwar92@gmail.com>
2022-03-28 17:36:41 +0530

    second commit by me

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
+print('*'*50)
 for i in range(10):
-    print('I Love you Swetha mam... because you my life become happy')
+    print('I Love you Swetha mam... because of you my life become happy')
+print('*'*50)
+print('i sent the picture to swetha mam about the github accounts')
+#print('*'*50)
